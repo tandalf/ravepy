498dd15592d99b128d11173402ded8746184c7a9
Timothy Ebiuwhe <timothy_ebiuwhe@live.com>
2018-02-21 19:31:51 +0100

    Added initial constants

---
+++ ravepy/constants/__init__.py
@@ -0,0 +1,25 @@
+"""
+Constants used in this library are found here. Majority are currency constants.
+"""
+
+#currencies
+NGN = 'NGN'
+USD = 'USD'
+KES = 'KES'
+EUR = 'EUR'
+GBP = 'GBP'
+GHS = 'GHS'
+ZAR = 'ZAR'
+
+#countries
+NIGERIA = 'NG'
+KENYA = 'KE'
+GHANA = 'GH'
+SOUTH_AFRICA = 'ZA'
+
+COUNTRY_CURRENCIES = {
+    NIGERIA: [NGN, USD, KES, EUR, GBP],
+    KENYA: [KES],
+    GHANA: [GHS, USD],
+    SOUTH_AFRICA: [ZAR]
+}
