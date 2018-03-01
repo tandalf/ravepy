212740f645892f2f9421b04d1a299140238cbe46
Timothy Ebiuwhe <timothy_ebiuwhe@live.com>
2018-03-01 20:01:57 +0100

    Uploaded to pypi

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+name = 'ravepy',
+packages = ['ravepy'],
+version = '0.2.0',
+description = 'Rave payment gateway client library for python',
+author = 'Timothy Ebiuwhe',
+author_email = 'timothy_ebiuwhe@live.com',
+url = 'https://github.com/tandalf/ravepy',
+download_url = 'https://github.com/tandalf/ravepy/archive/0.2.0.zip',
+keywords = ['ravepy', 'Rave', 'Rave Pay', 'Rave Python Client', 'Rave Gateway',
+    'Flutterwave Rave', 'python'],
+classifiers = [],
+install_requires=['request', 'pycrypto']
+)
