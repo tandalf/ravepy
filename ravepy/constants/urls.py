f6bae05da58ed15a7de177badd0027d10b9c6038
Timothy Ebiuwhe <timothy_ebiuwhe@live.com>
2018-03-07 21:37:43 +0100

    URL endpoints now separated for live and test environments

--- ravepy/constants/urls.py
+++ ravepy/constants/urls.py
@@ -6,27 +6,21 @@ URLS = namedtuple('URLS', ['DIRECT_CHARGE_URL', 'VALIDATE_CARD_CHARGE_URL',
     'TRANSACTION_VERIFICATION_XREQUERY_URL', 'PREAUTH_CHARGE_URL',
     'PREAUTH_CAPTURE_URL', 'PREAUTH_VOID_URL', 'GET_FEES_URL', 'BANKS_URL'])
 
+DEV_URL = 'http://flw-pms-dev.eu-west-1.elasticbeanstalk.com'
+PROD_URL = 'https://api.ravepay.co'
+
 dev_urls = URLS(
-    DIRECT_CHARGE_URL='http://flw-pms-dev.eu-west-1.elasticbeanstalk.com'\
-    '/flwv3-pug/getpaidx/api/charge',
-    VALIDATE_CARD_CHARGE_URL='http://flw-pms-dev.eu-west-1.elasticbeanstalk.com'\
-        '/flwv3-pug/getpaidx/api/validatecharge',
-    VALIDATE_ACCOUNT_CHARGE_URL='http://flw-pms-dev.eu-west-1.elasticbeanstalk.'\
-        'com/flwv3-pug/getpaidx/api/validate',
-    TRANSACTION_VERIFICATION_URL='http://flw-pms-dev.eu-west-1.elasticbeanstalk'\
-        '.com/flwv3-pug/getpaidx/api/verify',
-    TRANSACTION_VERIFICATION_XREQUERY_URL='http://flw-pms-dev.eu-west-1.'\
-        'elasticbeanstalk.com/flwv3-pug/getpaidx/api/xrequery',
-    PREAUTH_CHARGE_URL='http://flw-pms-dev.eu-west-1.elasticbeanstalk.com'\
-        '/flwv3-pug/getpaidx/api/charge',
-    PREAUTH_CAPTURE_URL='http://flw-pms-dev.eu-west-1.elasticbeanstalk.com'\
-        '/flwv3-pug/getpaidx/api/capture',
-    PREAUTH_VOID_URL='http://flw-pms-dev.eu-west-1.elasticbeanstalk.com'\
-        '/flwv3-pug/getpaidx/api/refundorvoid',
-    GET_FEES_URL='http://flw-pms-dev.eu-west-1.elasticbeanstalk.com'\
-        '/flwv3-pug/getpaidx/api/fee',
-    BANKS_URL='http://flw-pms-dev.eu-west-1.elasticbeanstalk.com'\
-        '/flwv3-pug/getpaidx/api/flwpbf-banks.js?json=1')
+    DIRECT_CHARGE_URL=DEV_URL + '/flwv3-pug/getpaidx/api/charge',
+    VALIDATE_CARD_CHARGE_URL=DEV_URL + '/flwv3-pug/getpaidx/api/validatecharge',
+    VALIDATE_ACCOUNT_CHARGE_URL=DEV_URL + '/flwv3-pug/getpaidx/api/validate',
+    TRANSACTION_VERIFICATION_URL=DEV_URL + '/flwv3-pug/getpaidx/api/verify',
+    TRANSACTION_VERIFICATION_XREQUERY_URL=DEV_URL + \
+        '/flwv3-pug/getpaidx/api/xrequery',
+    PREAUTH_CHARGE_URL=DEV_URL + '/flwv3-pug/getpaidx/api/charge',
+    PREAUTH_CAPTURE_URL=DEV_URL + '/flwv3-pug/getpaidx/api/capture',
+    PREAUTH_VOID_URL=DEV_URL + '/flwv3-pug/getpaidx/api/refundorvoid',
+    GET_FEES_URL=DEV_URL + '/flwv3-pug/getpaidx/api/fee',
+    BANKS_URL=DEV_URL + '/flwv3-pug/getpaidx/api/flwpbf-banks.js?json=1')
 
 
 
@@ -34,23 +28,14 @@ dev_urls = URLS(
 
 
 prod_urls = URLS(
-    DIRECT_CHARGE_URL='http://flw-pms-dev.eu-west-1.elasticbeanstalk.com'\
-    '/flwv3-pug/getpaidx/api/charge',
-    VALIDATE_CARD_CHARGE_URL='http://flw-pms-dev.eu-west-1.elasticbeanstalk.com'\
-        '/flwv3-pug/getpaidx/api/validatecharge',
-    VALIDATE_ACCOUNT_CHARGE_URL='http://flw-pms-dev.eu-west-1.elasticbeanstalk.'\
-        'com/flwv3-pug/getpaidx/api/validate',
-    TRANSACTION_VERIFICATION_URL='http://flw-pms-dev.eu-west-1.elasticbeanstalk'\
-        '.com/flwv3-pug/getpaidx/api/verify',
-    TRANSACTION_VERIFICATION_XREQUERY_URL='http://flw-pms-dev.eu-west-1.'\
-        'elasticbeanstalk.com/flwv3-pug/getpaidx/api/xrequery',
-    PREAUTH_CHARGE_URL='http://flw-pms-dev.eu-west-1.elasticbeanstalk.com'\
-        '/flwv3-pug/getpaidx/api/charge',
-    PREAUTH_CAPTURE_URL='http://flw-pms-dev.eu-west-1.elasticbeanstalk.com'\
-        '/flwv3-pug/getpaidx/api/capture',
-    PREAUTH_VOID_URL='http://flw-pms-dev.eu-west-1.elasticbeanstalk.com'\
-        '/flwv3-pug/getpaidx/api/refundorvoid',
-    GET_FEES_URL='http://flw-pms-dev.eu-west-1.elasticbeanstalk.com'\
-        '/flwv3-pug/getpaidx/api/fee',
-    BANKS_URL='http://flw-pms-dev.eu-west-1.elasticbeanstalk.com'\
-        '/flwv3-pug/getpaidx/api/flwpbf-banks.js?json=1')
+    DIRECT_CHARGE_URL=PROD_URL + '/flwv3-pug/getpaidx/api/charge',
+    VALIDATE_CARD_CHARGE_URL=PROD_URL + '/flwv3-pug/getpaidx/api/validatecharge',
+    VALIDATE_ACCOUNT_CHARGE_URL=PROD_URL + '/flwv3-pug/getpaidx/api/validate',
+    TRANSACTION_VERIFICATION_URL=PROD_URL + '/flwv3-pug/getpaidx/api/verify',
+    TRANSACTION_VERIFICATION_XREQUERY_URL=PROD_URL + \
+        '/flwv3-pug/getpaidx/api/xrequery',
+    PREAUTH_CHARGE_URL=PROD_URL + '/flwv3-pug/getpaidx/api/charge',
+    PREAUTH_CAPTURE_URL=PROD_URL + '/flwv3-pug/getpaidx/api/capture',
+    PREAUTH_VOID_URL=PROD_URL + '/flwv3-pug/getpaidx/api/refundorvoid',
+    GET_FEES_URL=PROD_URL + '/flwv3-pug/getpaidx/api/fee',
+    BANKS_URL=PROD_URL + '/flwv3-pug/getpaidx/api/flwpbf-banks.js?json=1')
