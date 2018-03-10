5d0a384e2d884caf9df271ce3caa8293418a4e4b
Timothy Ebiuwhe <timothy_ebiuwhe@live.com>
2018-03-10 22:18:21 +0100

    Added Fee factory

--- ravepy/__init__.py
+++ ravepy/__init__.py
@@ -2,15 +2,19 @@ from __future__ import absolute_import, unicode_literals
 
 from ravepy.resources.charge import ChargeFactory
 from ravepy.resources.banks import BankFactory
+from ravepy.resources.fees import FeeFactory
 from ravepy.constants import CARD, ACCOUNT
 
 __metaclass__ = type
 
 Charge = ChargeFactory(None)
 Bank = BankFactory(None)
+Fee = FeeFactory(None)
 
 def set_auth(auth_details):
     global Charge
     global Bank
+    global Fee
     Charge._auth_details = auth_details
     Bank._auth_details = auth_details
+    Fee._auth_details = auth_details
