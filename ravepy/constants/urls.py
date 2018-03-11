c56b341dec698be63bb031c8ae5ff61d2ac34f49
Timothy Ebiuwhe <timothy_ebiuwhe@live.com>
2018-03-11 13:46:23 +0100

    Refactored url constants

--- ravepy/constants/urls.py
+++ ravepy/constants/urls.py
@@ -9,33 +9,25 @@ URLS = namedtuple('URLS', ['DIRECT_CHARGE_URL', 'VALIDATE_CARD_CHARGE_URL',
 DEV_URL = 'http://flw-pms-dev.eu-west-1.elasticbeanstalk.com'
 PROD_URL = 'https://api.ravepay.co'
 
-dev_urls = URLS(
-    DIRECT_CHARGE_URL=DEV_URL + '/flwv3-pug/getpaidx/api/charge',
-    VALIDATE_CARD_CHARGE_URL=DEV_URL + '/flwv3-pug/getpaidx/api/validatecharge',
-    VALIDATE_ACCOUNT_CHARGE_URL=DEV_URL + '/flwv3-pug/getpaidx/api/validate',
-    TRANSACTION_VERIFICATION_URL=DEV_URL + '/flwv3-pug/getpaidx/api/verify',
-    TRANSACTION_VERIFICATION_XREQUERY_URL=DEV_URL + \
-        '/flwv3-pug/getpaidx/api/xrequery',
-    PREAUTH_CHARGE_URL=DEV_URL + '/flwv3-pug/getpaidx/api/charge',
-    PREAUTH_CAPTURE_URL=DEV_URL + '/flwv3-pug/getpaidx/api/capture',
-    PREAUTH_VOID_URL=DEV_URL + '/flwv3-pug/getpaidx/api/refundorvoid',
-    GET_FEES_URL=DEV_URL + '/flwv3-pug/getpaidx/api/fee',
-    BANKS_URL=DEV_URL + '/flwv3-pug/getpaidx/api/flwpbf-banks.js?json=1')
+url_kwargs = {
+    'DIRECT_CHARGE_URL': '/flwv3-pug/getpaidx/api/charge',
+    'VALIDATE_CARD_CHARGE_URL': '/flwv3-pug/getpaidx/api/validatecharge',
+    'VALIDATE_ACCOUNT_CHARGE_URL': '/flwv3-pug/getpaidx/api/validate',
+    'TRANSACTION_VERIFICATION_URL': '/flwv3-pug/getpaidx/api/verify',
+    'TRANSACTION_VERIFICATION_XREQUERY_URL': '/flwv3-pug/getpaidx/api/xrequery',
+    'PREAUTH_CHARGE_URL': '/flwv3-pug/getpaidx/api/charge',
+    'PREAUTH_CAPTURE_URL': '/flwv3-pug/getpaidx/api/capture',
+    'PREAUTH_VOID_URL': '/flwv3-pug/getpaidx/api/refundorvoi',
+    'GET_FEES_URL': '/flwv3-pug/getpaidx/api/fee',
+    'BANKS_URL': '/flwv3-pug/getpaidx/api/flwpbf-banks.js?json=1',
+}
 
+dev_url_kwargs = {}
+prod_url_kwargs = {}
 
+for url_name, url in url_kwargs:
+    dev_url_kwargs[url_name] = DEV_URL + url
+    prod_url_kwargs[url_name] = PROD_URL + url
 
-
-
-
-prod_urls = URLS(
-    DIRECT_CHARGE_URL=PROD_URL + '/flwv3-pug/getpaidx/api/charge',
-    VALIDATE_CARD_CHARGE_URL=PROD_URL + '/flwv3-pug/getpaidx/api/validatecharge',
-    VALIDATE_ACCOUNT_CHARGE_URL=PROD_URL + '/flwv3-pug/getpaidx/api/validate',
-    TRANSACTION_VERIFICATION_URL=PROD_URL + '/flwv3-pug/getpaidx/api/verify',
-    TRANSACTION_VERIFICATION_XREQUERY_URL=PROD_URL + \
-        '/flwv3-pug/getpaidx/api/xrequery',
-    PREAUTH_CHARGE_URL=PROD_URL + '/flwv3-pug/getpaidx/api/charge',
-    PREAUTH_CAPTURE_URL=PROD_URL + '/flwv3-pug/getpaidx/api/capture',
-    PREAUTH_VOID_URL=PROD_URL + '/flwv3-pug/getpaidx/api/refundorvoid',
-    GET_FEES_URL=PROD_URL + '/flwv3-pug/getpaidx/api/fee',
-    BANKS_URL=PROD_URL + '/flwv3-pug/getpaidx/api/flwpbf-banks.js?json=1')
+dev_urls = URLS(**dev_url_kwargs)
+prod_urls = URLS(**prod_url_kwargs)
